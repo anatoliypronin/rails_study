5cfcd6cbf26d25299dcc5c43ed36ffd8d63cf485
AlexeyRyabchikov <al.ryabchikov@gmail.com>
2020-04-06 20:15:55 +0400

    Add email_validator

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,7 @@
 class Admin < ApplicationRecord
   validates :name, :email, :password_digest, :role, :state, presence: true
   validates :name, length: { in: 2..20 }
-  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, uniqueness: true, email: true
   validates :password_digest, length: { in: 4..12 }
   validates :role, inclusion: { in: %w(admin editor),
     message: `%{value} must be admin or editor` }
