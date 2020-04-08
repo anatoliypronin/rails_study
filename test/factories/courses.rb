9e8d29cdebd1b45e8389c852ecd522e2613cdae5
AlexeyRyabchikov <al.ryabchikov@gmail.com>
2020-04-08 13:19:38 +0400

    Fixed courses.rb

--- test/factories/courses.rb
+++ test/factories/courses.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :course do
-    title { "title_title_title" }
-    description { "description_description_description_description_description_description_description_description" }
-    state { "active" }
+    title { 'title' }
+    description { 'description' }
+    state { 'active' }
   end
 end
