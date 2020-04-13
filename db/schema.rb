32a37f747862fd19b5bd024071bdcf86f3ad18a8
Salikov <roma.salikov99@yandex.ru>
2020-04-13 21:24:25 +0400

    add unique for student on validete and db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_065914) do
+ActiveRecord::Schema.define(version: 2020_04_13_171913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,17 @@ ActiveRecord::Schema.define(version: 2020_04_08_065914) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "student_courses", force: :cascade do |t|
+    t.bigint "student_id", null: false
+    t.bigint "course_id", null: false
+    t.boolean "completed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_id"], name: "index_student_courses_on_course_id"
+    t.index ["student_id", "course_id"], name: "index_student_courses_on_student_id_and_course_id", unique: true
+    t.index ["student_id"], name: "index_student_courses_on_student_id"
+  end
+
   create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -49,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_04_08_065914) do
     t.string "state"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_students_on_email", unique: true
   end
 
   create_table "teachers", force: :cascade do |t|
@@ -62,4 +74,6 @@ ActiveRecord::Schema.define(version: 2020_04_08_065914) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "student_courses", "courses"
+  add_foreign_key "student_courses", "students"
 end
