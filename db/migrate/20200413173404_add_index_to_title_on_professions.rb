e7c0776c0191c8bd083d4b1bee1c77bc82b9f2c9
Salikov <roma.salikov99@yandex.ru>
2020-04-13 22:04:23 +0400

    delete array in migration for add_index

--- db/migrate/20200413173404_add_index_to_title_on_professions.rb
+++ db/migrate/20200413173404_add_index_to_title_on_professions.rb
@@ -1,5 +1,5 @@
 class AddIndexToTitleOnProfessions < ActiveRecord::Migration[6.0]
   def change
-    add_index :professions, [:title], unique: true
+    add_index :professions, :title, unique: true
   end
 end
