b1656ce51118e7434b9a2e706ce5f73d6cbe92c2
Salikov <roma.salikov99@yandex.ru>
2020-04-13 20:53:23 +0400

    fix test student_course

--- test/models/student_course_test.rb
+++ test/models/student_course_test.rb
@@ -1,13 +1,16 @@
 require 'test_helper'
 
 class StudentCourseTest < ActiveSupport::TestCase
-  test 'should create StudentCourse' do
+  test 'should create StudentCourse and check whether the student and course are the same' do
     student = create :student
-    create :course
+    course = create :course
 
-    student.courses << Course.last
+    student.courses << course
 
     created_student_course = StudentCourse.last
     assert created_student_course
+
+    assert_equal student.id, created_student_course.student_id
+    assert_equal course.id, created_student_course.course_id
   end
 end
