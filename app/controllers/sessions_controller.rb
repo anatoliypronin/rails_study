f35b65ad1236275c52b856a2941a38eee690d5a6
AlexeyRyabchikov <al.ryabchikov@gmail.com>
2020-04-21 20:05:32 +0400

    Fixed index.html.haml and session_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
     admin = Admin.find_by(email: params[:admin][:email])
     if admin&.authenticate(params[:admin][:password])
       admin_sign_in(admin)
-      session[:admin_id] = admin.id
       redirect_to admin_root_path
     else
       render action: :new
