f6dd403fac19fd5e2d82c1a0a9f1c697bd66dc93
Salikov <roma.salikov99@yandex.ru>
2020-04-27 16:07:03 +0400

    add decorate on root page teacher

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'rubocop-rails', require: false
 gem 'russian', '~> 0.6.0'
 gem 'simple_form'
 gem 'state_machines-activerecord'
+gem 'draper'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/controllers/teacher/welcome_controller.rb
+++ app/controllers/teacher/welcome_controller.rb
@@ -1,6 +1,6 @@
 class Teacher::WelcomeController < Teacher::ApplicationController
   def index
-    @courses = Course.all
+    @courses = Course.all.decorate
     # необходимо добавить 2 модели article adn StudentHomework
     # @articles = Article.all
     # @student_homework = StudentHomework.all
