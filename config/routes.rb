3764b9434d19f5862c23b86e1be1f6a464c0d921
Salikov <roma.salikov99@yandex.ru>
2020-04-27 12:15:45 +0400

    replacing destroy with changing the teachers state(active and deleted)

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,10 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'admins#index'
-    resources :admins do
+    resources :admins, :students, :courses, :teachers do
       put 'restore'
       put 'del'
     end
     resources :professions
-    resources :teachers
-    resources :students
-    resources :courses do
-      put 'restore'
-      put 'del'
-    end
   end
 end
