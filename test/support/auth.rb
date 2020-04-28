ee5db3ceec6e930b0a0523776af2389b6bc2ee6e
Salikov <roma.salikov99@yandex.ru>
2020-04-28 19:29:12 +0400

    update for the admin session

--- test/support/auth.rb
+++ test/support/auth.rb
@@ -1,7 +1,7 @@
 module Support
   module Auth
     def sign_in_as_admin(admin)
-      post session_path, params: { admin: { email: admin.email, password: admin.password } }
+      post admin_session_path, params: { admin: { email: admin.email, password: admin.password } }
     end
   end
 end
