fa9890c77d266eb4fcf0f0195caa3e2ce3801ac0
Ekaterina Kulakova <sharpeika334@gmail.com>
2020-05-01 21:02:31 +0400

    added null: false parameter for required fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_000230) do
+ActiveRecord::Schema.define(version: 2020_05_01_154542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,9 +47,9 @@ ActiveRecord::Schema.define(version: 2020_04_22_000230) do
   end
 
   create_table "lessons", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.text "homework"
+    t.string "title", null: false
+    t.text "description", null: false
+    t.text "homework", null: false
     t.string "state"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
--- test/models/lesson_test.rb
+++ test/models/lesson_test.rb
@@ -1,12 +1,29 @@
 require 'test_helper'
 
 class LessonTest < ActiveSupport::TestCase
-  test 'should create lesson' do
-    lesson = build :lesson
+
+  test 'should create course' do
+    lesson = build :lesson, state: "any state"
+    lesson.save
+    p lesson
+    created_lesson = Lesson.last
+    assert created_lesson
+  end
+
+  test 'should not create lesson with title lenght > 50' do
+    lesson = build :lesson, title: 'Title' * 20
     lesson.save
 
     created_lesson = Lesson.last
-    assert created_lesson
+    assert_not created_lesson
+  end
+
+  test 'should not create lesson with invalid state' do
+    lesson = build :lesson, state: nil
+    lesson.save
+
+    created_lesson = Lesson.last
+    assert_not created_lesson
   end
 
   test 'should not create lesson with title length > 50' do
@@ -17,11 +34,5 @@ class LessonTest < ActiveSupport::TestCase
     assert_not created_lesson
   end
 
-  test 'should not create lesson with incorrect state' do
-    lesson = build :student, state: 'any state'
-    lesson.save
 
-    created_lesson = Lesson.last
-    assert_not created_lesson
-  end
 end
