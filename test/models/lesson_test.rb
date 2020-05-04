0bd5f29783e82387612571b04da6c7649fc1d522
Ekaterina Kulakova <sharpeika334@gmail.com>
2020-05-04 21:04:17 +0400

    Added tests

--- test/models/lesson_test.rb
+++ test/models/lesson_test.rb
@@ -5,6 +5,7 @@ class LessonTest < ActiveSupport::TestCase
   test 'should create course' do
     lesson = build :lesson
     lesson.save
+
     created_lesson = Lesson.last
     assert created_lesson
   end
@@ -23,5 +24,21 @@ class LessonTest < ActiveSupport::TestCase
 
     created_lesson = Lesson.last
     assert_not created_lesson
+  end
+
+  test 'should del lesson' do
+    lesson = create :lesson
+    lesson.del
+
+    lesson.reload
+    assert_equal "deleted", lesson.state
+  end
+
+  test 'should restore lesson' do
+    lesson = create :lesson
+    lesson.restore
+
+    lesson.reload
+    assert_equal "active", lesson.state
   end
 end
