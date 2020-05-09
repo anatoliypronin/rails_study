88566348361f31e06f843bf95dfb18be7099951c
Irina Ablyazova <ulinvest73@mail.ru>
2020-05-09 21:55:18 +0300

    change rubocop settings, add index to email for teacher in db. fix rubocop problems

--- app/models/student_homework.rb
+++ app/models/student_homework.rb
@@ -1,7 +1,8 @@
 class StudentHomework < ApplicationRecord
   belongs_to :student
   # belongs_to :lesson
-  validates :raiting, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :raiting, presence: true,
+                      numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
   validates :date_begin, presence: true
   validates :date_end, presence: true
   validates :link_homework, presence: true, url: true
