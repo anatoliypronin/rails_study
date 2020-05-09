17ac1923ee759b3573ad1dae594e50274c15688d
Irina Ablyazova <ulinvest73@mail.ru>
2020-05-09 13:02:58 +0300

    student_homework_model

--- app/models/student_homework.rb
+++ app/models/student_homework.rb
@@ -2,21 +2,14 @@ class StudentHomework < ApplicationRecord
   belongs_to :student
   # belongs_to :lesson
   validates :raiting, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
-  # validates :student_comment
-  # validates :teacher_comment
   validates :date_begin, presence: true
   validates :date_end, presence: true
   validates :link_homework, presence: true, url: true
   # validates :student, uniqueness: { scope: :lesson }
-  state_machine initial: :recived do
-    state :recived
+  state_machine initial: :doing do
     state :doing
     state :checking
     state :accepted
-
-    event :begin do
-      transition recived: :doing
-    end
 
     event :testing do
       transition doing: :checking
