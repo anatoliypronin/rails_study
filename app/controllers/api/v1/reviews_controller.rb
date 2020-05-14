05dd4b62cbb8cea2743347a6abdc28d3a439b4a0
sanyk15 <sanekmakienko322@gmail.com>
2020-05-14 20:24:44 +0400

    add controllers into api

---
+++ app/controllers/api/v1/reviews_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::ReviewsController < Api::V1::ApplicationController
+    def index
+      reviews = Review.active.order(title: :asc)
+      render json: reviews, each_serializer: ReviewIndexSerializer
+    end
+  
+    def show
+      review = Review.active.find(params[:id])
+      render json: review, serializer: ReviewShowSerializer
+    end
+  end
+  
