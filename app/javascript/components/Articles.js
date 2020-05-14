1a1811d13d2176f205b6af98cc79c0dd8abaf0f7
sanyk15 <sanekmakienko322@gmail.com>
2020-05-14 22:46:09 +0400

    install react-rails

---
+++ app/javascript/components/Articles.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Articles extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Статьи))</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Articles
