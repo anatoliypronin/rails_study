369dec23efaff06103bf23f44b01dc175dcd1dda
sanyk15 <sanekmakienko322@gmail.com>
2020-05-14 20:32:22 +0400

    rubocop fixing

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,12 +1,11 @@
 class Api::V1::ArticlesController < Api::V1::ApplicationController
-    def index
-      articles = Article.active.order(title: :asc)
-      render json: articles, each_serializer: ArticleIndexSerializer
-    end
-  
-    def show
-      article = Article.active.find(params[:id])
-      render json: article, serializer: ArticleShowSerializer
-    end
+  def index
+    articles = Article.active.order(title: :asc)
+    render json: articles, each_serializer: ArticleIndexSerializer
   end
-  
+
+  def show
+    article = Article.active.find(params[:id])
+    render json: article, serializer: ArticleShowSerializer
+  end
+end
