99f71cbc0be3d2e20ae1bb915f6805d432c20381
sanyk15 <sanekmakienko322@gmail.com>
2020-05-18 11:36:00 +0400

    Revert "fix Articles component"
    This reverts commit 6f02a9b09d34569bdeb24d981d880a1fdcb73c48.

--- app/javascript/components/Articles.js
+++ app/javascript/components/Articles.js
@@ -1,65 +1,41 @@
 import React from "react"
 import PropTypes from "prop-types"
 class Articles extends React.Component {
-  article_id = null;
-
   constructor(props) {
     super(props)
     this.state = { articles: [] }
-    this.article_id = null
   };
-
-  setArticleId(id) {
-    this.article_id = id
-    this.componentDidMount()
-  }
 
   fetchArticles() {
     fetch("/api/v1/articles")
       .then(response => response.json())
       .then(result => this.setState({ articles: result }))
-    this.article_id = null
   };
 
-  fetchArticle() {
-    fetch("/api/v1/articles/" + this.article_id)
-      .then(response => response.json())
-      .then(result => this.setState({ articles: result }))
-  }
-
   componentDidMount() {
-    if(this.article_id == null)
-      this.fetchArticles()
-    else
-      this.fetchArticle(this.article_id)
+    this.fetchArticles()
   };
 
   render() {
-    if(this.article_id != null){
-      return <h1>{this.state.articles.title}</h1>;
-    } else {
-      return (
-        <React.Fragment>
-          <h1>Статьи</h1>
-          <table className="table table-hover">
+    return (
+      <React.Fragment>
+        <h1>Статьи</h1>
+        <table className="table table-hover">
+          <tr>
+            <th>Заголовок</th>
+            <th>Текст</th>
+            <th>Дата обновления</th>
+          </tr>
+          {this.state.articles.map(article =>
             <tr>
-              <th>Заголовок</th>
-              <th>Текст</th>
-              <th>Дата обновления</th>
-              <th></th>
+              <td>{article.title}</td>
+              <td>{article.body}</td>
+              <td>{article.updated_at}</td>
             </tr>
-            {this.state.articles.map(article =>
-              <tr>
-                <td>{article.title}</td>
-                <td>{article.body}</td>
-                <td>{article.updated_at}</td>
-                <td><button onClick={this.setArticleId(article.id)}>Показать</button></td>
-              </tr>
-            )}
-          </table>
-        </React.Fragment>
-      );
-    }
+          )}
+        </table>
+      </React.Fragment>
+    );
   }
 }
 
