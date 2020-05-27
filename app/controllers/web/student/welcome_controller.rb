5a911ca0ee45fedbc45409ad1aedb9f458c68d30
AlexeyRyabchikov <al.ryabchikov@gmail.com>
2020-05-27 18:54:00 +0400

    Fixed teacher_student_homework

--- app/controllers/web/student/welcome_controller.rb
+++ app/controllers/web/student/welcome_controller.rb
@@ -1,5 +1,5 @@
 class Web::Student::WelcomeController < Web::Student::ApplicationController
   def index
-    @courses = Course.where(state: 'active').decorate
+    @courses = Course.where(state: 'active').order(updated_at: :desc).decorate
   end
 end
