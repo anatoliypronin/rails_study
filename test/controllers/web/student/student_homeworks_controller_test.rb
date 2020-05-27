2b7e2ab25c277eaf3ffe9f512c913bfde8ea85b9
sanyk15 <sanekmakienko322@gmail.com>
2020-05-27 22:55:04 +0400

    add tests

---
+++ test/controllers/web/student/student_homeworks_controller_test.rb
@@ -0,0 +1,35 @@
+require "test_helper"
+
+class Web::Student::StudentHomeworksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @student_homework = create :student_homework
+    @student = @student_homework.student
+    sign_in_as_student(@student)
+  end
+
+  test "should get index student_homeworks page" do
+    get student_student_homeworks_path
+    assert_response :success
+  end
+
+  test "should get show student_homework page" do
+    get student_student_homework_path(@student_homework)
+    assert_response :success
+  end
+
+  test "should get edit student_homework page" do
+    get edit_student_student_homework_path(@student_homework)
+    assert_response :success
+  end
+
+  test "should put update student_homework" do
+    attrs = {}
+    attrs[:link_homework] = generate :link_homework
+
+    put student_student_homework_path(@student_homework.id), params: { student_homework: attrs }
+    assert_response :redirect
+
+    @student_homework.reload
+    assert_equal attrs[:link_homework], @student_homework.link_homework
+  end
+end
