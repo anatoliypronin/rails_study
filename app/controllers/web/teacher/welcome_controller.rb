57301ed4f6abbfe39019c3a24ddcacbabdc08bef
AlexeyRyabchikov <al.ryabchikov@gmail.com>
2020-05-28 15:39:31 +0400

    Fixed teacher_welcome_page

--- app/controllers/web/teacher/welcome_controller.rb
+++ app/controllers/web/teacher/welcome_controller.rb
@@ -1,7 +1,7 @@
 class Web::Teacher::WelcomeController < Web::Teacher::ApplicationController
   def index
-    @courses = Course.where(teacher: current_teacher).decorate
-    @articles = Article.decorate
-    @student_homeworks = StudentHomework.where(lesson_id: current_teacher.lessons.ids).decorate
+    @courses = Course.where(teacher: current_teacher).order(updated_at: :desc).decorate.first(3)
+    @student_homeworks = StudentHomework.where(lesson_id: current_teacher.lessons.ids).order(updated_at: :desc).decorate.first(3)
+    @articles = Article.where(author: current_teacher).order(updated_at: :desc).decorate.first(3)
   end
 end
