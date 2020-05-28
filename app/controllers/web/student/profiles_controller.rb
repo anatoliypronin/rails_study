45aa60f4b7d293e8907e1016bc0828a5af34d502
Irina Ablyazova <ulinvest73@mail.ru>
2020-05-28 22:39:29 +0300

    add translations

--- app/controllers/web/student/profiles_controller.rb
+++ app/controllers/web/student/profiles_controller.rb
@@ -15,6 +15,7 @@ class Web::Student::ProfilesController < Web::Student::ApplicationController
 
   def show
     @student = current_student.decorate
+    @hash_graph = hash_graph(@student)
   end
 
   def delete_photo
@@ -24,6 +25,15 @@ class Web::Student::ProfilesController < Web::Student::ApplicationController
   end
 
   private
+
+  def hash_graph(student)
+    result = {}
+    student.student_homeworks.where.not(raiting: nil).each do |sh|
+      lesson = Lesson.find(sh.lesson_id)
+      result[lesson.title] = sh.raiting
+    end
+    result
+  end
 
   def student_attrs
     params.require(:student).permit(:first_name, :last_name, :phone_number, :email, :student_id, :photo)
--- app/controllers/web/teacher/welcome_controller.rb
+++ app/controllers/web/teacher/welcome_controller.rb
@@ -2,6 +2,17 @@ class Web::Teacher::WelcomeController < Web::Teacher::ApplicationController
   def index
     @courses = Course.where(teacher: current_teacher).decorate
     @articles = Article.decorate
-    @student_homeworks = StudentHomework.where(lesson_id: current_teacher.lessons.ids).decorate
+    @student_homeworks = StudentHomework.where(lesson_id: current_teacher.lessons.ids).order(:lesson_id)
+    @hash_graph = hash_graph
+  end
+
+  def hash_graph
+    result = {}
+    @student_homeworks.where.not(raiting: nil).each do |sh|
+      lesson = Lesson.find(sh.lesson_id)
+      avg_raiting = @student_homeworks.where.not(raiting: nil).where(lesson_id: sh.lesson_id).sum(:raiting) / @student_homeworks.where.not(raiting: nil).where(lesson_id: sh.lesson_id).count
+      result[lesson.title] = avg_raiting
+    end
+    result
   end
 end
