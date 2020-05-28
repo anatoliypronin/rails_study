ae71cba4279cff22dc41d17fef2f03df8ba4602b
Irina Ablyazova <ulinvest73@mail.ru>
2020-05-28 23:01:36 +0300

    fix merge changes

--- app/controllers/web/teacher/welcome_controller.rb
+++ app/controllers/web/teacher/welcome_controller.rb
@@ -3,14 +3,15 @@ class Web::Teacher::WelcomeController < Web::Teacher::ApplicationController
     @courses = Course.where(teacher: current_teacher).order(updated_at: :desc).decorate.first(3)
     @student_homeworks = StudentHomework.where(lesson_id: current_teacher.lessons.ids).order(updated_at: :desc).decorate.first(3)
     @articles = Article.where(author: current_teacher).order(updated_at: :desc).decorate.first(3)
-    @hash_graph = hash_graph
+    student_homeworks_graph = StudentHomework.where(lesson_id: current_teacher.lessons.ids).order(:lesson_id)
+    @hash_graph = hash_graph(student_homeworks_graph)
   end
 
-  def hash_graph
+  def hash_graph(student_homeworks)
     result = {}
-    @student_homeworks.where.not(raiting: nil).each do |sh|
+    student_homeworks.each do |sh|
       lesson = Lesson.find(sh.lesson_id)
-      avg_raiting = @student_homeworks.where.not(raiting: nil).where(lesson_id: sh.lesson_id).sum(:raiting) / @student_homeworks.where.not(raiting: nil).where(lesson_id: sh.lesson_id).count
+      avg_raiting = student_homeworks.where.not(raiting: nil).where(lesson_id: sh.lesson_id).sum(:raiting) / student_homeworks.where.not(raiting: nil).where(lesson_id: sh.lesson_id).count
       result[lesson.title] = avg_raiting
     end
     result
