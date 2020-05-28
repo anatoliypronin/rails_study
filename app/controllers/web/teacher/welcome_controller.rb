bb78c80d8e972198016e3cd7143e7c40d36dd664
Irina Ablyazova <ulinvest73@mail.ru>
2020-05-29 02:05:03 +0300

    fix rubocop mistakes

--- app/controllers/web/teacher/welcome_controller.rb
+++ app/controllers/web/teacher/welcome_controller.rb
@@ -3,8 +3,7 @@ class Web::Teacher::WelcomeController < Web::Teacher::ApplicationController
     @courses = Course.where(teacher: current_teacher).order(updated_at: :desc).decorate.first(3)
     @student_homeworks = StudentHomework.where(lesson_id: current_teacher.lessons.ids).order(updated_at: :desc).decorate.first(3)
     @articles = Article.where(author: current_teacher).order(updated_at: :desc).decorate.first(3)
-    # student_homeworks_graph = StudentHomework.where(lesson_id: current_teacher.lessons.ids).where.not(raiting: nil).order(:lesson_id)
-    student_homeworks_graph = StudentHomework.where("lesson_id IN (?) AND raiting IS NOT null", current_teacher.lessons.ids).order(:lesson_id)
+    student_homeworks_graph = StudentHomework.where('lesson_id IN (?) AND raiting IS NOT null', current_teacher.lessons.ids).order(:lesson_id)
     @hash_graph = hash_graph(student_homeworks_graph)
   end
 
